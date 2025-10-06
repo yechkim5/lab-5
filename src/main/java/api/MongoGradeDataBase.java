5c5d5824cefc3f412b6b8e2e0a3be741d3f61697
rohanuoft <rohan.kuan@mail.utoronto.ca>
2025-10-06 12:55:33 -0400

    Added task 3b

--- src/main/java/api/MongoGradeDataBase.java
+++ src/main/java/api/MongoGradeDataBase.java
@@ -261,8 +261,36 @@ public class MongoGradeDataBase implements GradeDataBase {
                 .addHeader(CONTENT_TYPE, APPLICATION_JSON)
                 .build();
 
-        final Response response;
-        final JSONObject responseBody;
+        public Team getMyTeam() {
+        final OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        final Request request = new Request.Builder()
+                .url(String.format("%s/team", API_URL))
+                .method("GET", null)
+                .addHeader(TOKEN, getAPIToken())
+                .addHeader(CONTENT_TYPE, APPLICATION_JSON)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt(STATUS_CODE) == SUCCESS_CODE) {
+                final JSONObject team = responseBody.getJSONObject("team");
+                final JSONArray membersArray = team.getJSONArray("members");
+                final String[] members = new String[membersArray.length()];
+                for (int i = 0; i < membersArray.length(); i++) {
+                    members[i] = membersArray.getString(i);
+                }
+                return Team.builder()
+                        .name(team.getString(NAME))
+                        .members(members)
+                        .build();
+            } else {
+                throw new RuntimeException(responseBody.getString(MESSAGE));
+            }
+        } catch (IOException | JSONException event) {
+            throw new RuntimeException(event);
+        }
 
         // TODO Task 3b: Implement the logic to get the team information
         // HINT 1: Look at the formTeam method to get an idea on how to parse the response
